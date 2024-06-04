a88f480a1278f332e7b6c9c36e582ac194b31eaf
Pham Tu <tubackkhoa@gmail.com>
2024-06-04 16:45:24 +0700

    remove redundant libraries

--- src/msg.rs
+++ src/msg.rs
@@ -1,5 +1,5 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::{Addr, Uint128, Uint256};
+use cosmwasm_std::Addr;
 use cw721::Cw721ReceiveMsg;
 
 use crate::interface::AssetInfo;
