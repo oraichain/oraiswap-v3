ac212a602280b3a3ae50e619de204aa8e0ed3528
Pham Tu <tubackkhoa@gmail.com>
2024-06-10 10:38:09 +0700

    add test_swap not enough liquidity

--- contracts/oraiswap-v3/src/tests/swap.rs
+++ contracts/oraiswap-v3/src/tests/swap.rs
@@ -1,4 +1,5 @@
 use cosmwasm_std::coin;
+
 use decimal::{Decimal, Factories};
 
 use crate::{
@@ -20,9 +21,9 @@ fn test_swap_x_to_y() {
     let mut app = MockApp::new(&[("alice", &[coin(initial_amount, "orai")])]);
     app.set_token_contract(Box::new(crate::create_entry_points_testing!(cw20_base)));
     let fee_tier = FeeTier::new(protocol_fee, 10).unwrap();
-    let clmm_addr = app.create_dex("alice", protocol_fee).unwrap();
+    let dex = app.create_dex("alice", protocol_fee).unwrap();
 
-    add_fee_tier!(app, clmm_addr, fee_tier, "alice").unwrap();
+    add_fee_tier!(app, dex, fee_tier, "alice").unwrap();
 
     let init_tick = 0;
     let init_sqrt_price = calculate_sqrt_price(init_tick).unwrap();
@@ -31,7 +32,7 @@ fn test_swap_x_to_y() {
 
     create_pool!(
         app,
-        clmm_addr,
+        dex,
         token_x,
         token_y,
         fee_tier,
@@ -41,8 +42,8 @@ fn test_swap_x_to_y() {
     )
     .unwrap();
 
-    approve!(app, token_x, clmm_addr, initial_amount, "alice").unwrap();
-    approve!(app, token_y, clmm_addr, initial_amount, "alice").unwrap();
+    approve!(app, token_x, dex, initial_amount, "alice").unwrap();
+    approve!(app, token_y, dex, initial_amount, "alice").unwrap();
 
     let pool_key = PoolKey::new(token_x.clone(), token_y.clone(), fee_tier).unwrap();
 
@@ -54,7 +55,7 @@ fn test_swap_x_to_y() {
 
     create_position!(
         app,
-        clmm_addr,
+        dex,
         pool_key,
         lower_tick_index,
         upper_tick_index,
@@ -67,7 +68,7 @@ fn test_swap_x_to_y() {
 
     create_position!(
         app,
-        clmm_addr,
+        dex,
         pool_key,
         lower_tick_index - 20,
         middle_tick_index,
@@ -78,7 +79,7 @@ fn test_swap_x_to_y() {
     )
     .unwrap();
 
-    let pool = get_pool!(app, clmm_addr, token_x, token_y, fee_tier).unwrap();
+    let pool = get_pool!(app, dex, token_x, token_y, fee_tier).unwrap();
 
     assert_eq!(pool.liquidity, liquidity_delta);
 
@@ -86,19 +87,19 @@ fn test_swap_x_to_y() {
     let swap_amount = TokenAmount(amount);
 
     mint!(app, token_x, "bob", amount, "alice").unwrap();
-    approve!(app, token_x, clmm_addr, amount, "bob").unwrap();
+    approve!(app, token_x, dex, amount, "bob").unwrap();
 
     let slippage = SqrtPrice::new(MIN_SQRT_PRICE);
-    let target_sqrt_price = quote!(app, clmm_addr, pool_key, true, swap_amount, true, slippage)
+    let target_sqrt_price = quote!(app, dex, pool_key, true, swap_amount, true, slippage)
         .unwrap()
         .target_sqrt_price;
 
-    let before_dex_x = balance_of!(app, token_x, clmm_addr);
-    let before_dex_y = balance_of!(app, token_y, clmm_addr);
+    let before_dex_x = balance_of!(app, token_x, dex);
+    let before_dex_y = balance_of!(app, token_y, dex);
 
     swap!(
         app,
-        clmm_addr,
+        dex,
         pool_key,
         true,
         swap_amount,
@@ -109,17 +110,17 @@ fn test_swap_x_to_y() {
     .unwrap();
 
     // Load states
-    let pool = get_pool!(app, clmm_addr, token_x, token_y, fee_tier).unwrap();
-    let lower_tick = get_tick!(app, clmm_addr, pool_key, lower_tick_index).unwrap();
-    let middle_tick = get_tick!(app, clmm_addr, pool_key, middle_tick_index).unwrap();
-    let upper_tick = get_tick!(app, clmm_addr, pool_key, upper_tick_index).unwrap();
-    let lower_tick_bit = is_tick_initialized!(app, clmm_addr, pool_key, lower_tick_index);
-    let middle_tick_bit = is_tick_initialized!(app, clmm_addr, pool_key, middle_tick_index);
-    let upper_tick_bit = is_tick_initialized!(app, clmm_addr, pool_key, upper_tick_index);
+    let pool = get_pool!(app, dex, token_x, token_y, fee_tier).unwrap();
+    let lower_tick = get_tick!(app, dex, pool_key, lower_tick_index).unwrap();
+    let middle_tick = get_tick!(app, dex, pool_key, middle_tick_index).unwrap();
+    let upper_tick = get_tick!(app, dex, pool_key, upper_tick_index).unwrap();
+    let lower_tick_bit = is_tick_initialized!(app, dex, pool_key, lower_tick_index);
+    let middle_tick_bit = is_tick_initialized!(app, dex, pool_key, middle_tick_index);
+    let upper_tick_bit = is_tick_initialized!(app, dex, pool_key, upper_tick_index);
     let bob_x = balance_of!(app, token_x, "bob");
     let bob_y = balance_of!(app, token_y, "bob");
-    let dex_x = balance_of!(app, token_x, clmm_addr);
-    let dex_y = balance_of!(app, token_y, clmm_addr);
+    let dex_x = balance_of!(app, token_x, dex);
+    let dex_y = balance_of!(app, token_y, dex);
     let delta_dex_y = before_dex_y - dex_y;
     let delta_dex_x = dex_x - before_dex_x;
     let expected_y = amount - 10;
@@ -304,4 +305,93 @@ fn test_swap_y_to_x() {
     assert!(lower_tick_bit);
     assert!(middle_tick_bit);
     assert!(upper_tick_bit);
+}
+
+#[test]
+fn test_swap_not_enough_liquidity_token_x() {
+    let initial_amount = 10u128.pow(10);
+    let protocol_fee = Percentage::from_scale(6, 3);
+    let mut app = MockApp::new(&[("alice", &[coin(initial_amount, "orai")])]);
+    app.set_token_contract(Box::new(crate::create_entry_points_testing!(cw20_base)));
+    let dex = app.create_dex("alice", protocol_fee).unwrap();
+
+    let (token_x, token_y) = create_tokens!(app, initial_amount, initial_amount);
+
+    let fee_tier = FeeTier::new(protocol_fee, 10).unwrap();
+
+    add_fee_tier!(app, dex, fee_tier, "alice").unwrap();
+
+    let init_tick = 0;
+    let init_sqrt_price = calculate_sqrt_price(init_tick).unwrap();
+    create_pool!(
+        app,
+        dex,
+        token_x,
+        token_y,
+        fee_tier,
+        init_sqrt_price,
+        init_tick,
+        "alice"
+    )
+    .unwrap();
+
+    approve!(app, token_x, dex, initial_amount, "alice").unwrap();
+    approve!(app, token_y, dex, initial_amount, "alice").unwrap();
+
+    let pool_key = PoolKey::new(token_x.clone(), token_y.clone(), fee_tier).unwrap();
+
+    let lower_tick_index = -10;
+    let middle_tick_index = 10;
+    let upper_tick_index = 20;
+
+    let liquidity_delta = Liquidity::from_integer(1000000);
+
+    create_position!(
+        app,
+        dex,
+        pool_key,
+        lower_tick_index,
+        upper_tick_index,
+        liquidity_delta,
+        SqrtPrice::new(0),
+        SqrtPrice::max_instance(),
+        "alice"
+    )
+    .unwrap();
+
+    create_position!(
+        app,
+        dex,
+        pool_key,
+        middle_tick_index,
+        upper_tick_index + 20,
+        liquidity_delta,
+        SqrtPrice::new(0),
+        SqrtPrice::max_instance(),
+        "alice"
+    )
+    .unwrap();
+
+    let pool = get_pool!(app, dex, token_x, token_y, fee_tier).unwrap();
+
+    assert_eq!(pool.liquidity, liquidity_delta);
+
+    let amount = 1000;
+    let swap_amount = TokenAmount(amount);
+    mint!(app, token_x, "bob", amount, "alice").unwrap();
+    approve!(app, token_x, dex, amount, "bob").unwrap();
+
+    let target_sqrt_price = SqrtPrice::new(MIN_SQRT_PRICE);
+
+    let error = swap!(
+        app,
+        dex,
+        pool_key,
+        true,
+        swap_amount,
+        true,
+        target_sqrt_price,
+        "bob"
+    )
+    .unwrap_err();
 }
