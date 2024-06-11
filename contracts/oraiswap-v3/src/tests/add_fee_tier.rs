55dc4afb66a091cd2db995aad82f8e04605e1a72
vuonghuuhung <vuonghuuhung2002@gmail.com>
2024-06-11 09:58:03 +0700

    use macros

--- contracts/oraiswap-v3/src/tests/add_fee_tier.rs
+++ contracts/oraiswap-v3/src/tests/add_fee_tier.rs
@@ -1,26 +1,23 @@
 use crate::math::types::percentage::Percentage;
 use crate::msg::QueryMsg;
-use crate::tests::helper::MockApp;
+use crate::tests::helper::{macros::*, MockApp};
 use crate::FeeTier;
 use decimal::Decimal;
 
 #[test]
 fn test_add_multiple_fee_tiers() {
-    let mut mock_app = MockApp::new(&[("admin", &[])]);
-    let owner = "admin";
-
-    let protocol_fee = Percentage::new(0);
-    let dex_addr = mock_app.create_dex(owner, protocol_fee).unwrap();
+    let mut app: MockApp = MockApp::new(&[("alice", &[])]);
+    let dex = create_dex!(app, Percentage::new(0));
 
     let first_fee_tier = FeeTier::new(Percentage::new(1), 1).unwrap();
     let second_fee_tier = FeeTier::new(Percentage::new(1), 2).unwrap();
     let third_fee_tier = FeeTier::new(Percentage::new(1), 4).unwrap();
 
-    mock_app.add_fee_tier(owner, dex_addr.as_str(), first_fee_tier.clone()).unwrap();
-    mock_app.add_fee_tier(owner, dex_addr.as_str(), second_fee_tier.clone()).unwrap();
-    mock_app.add_fee_tier(owner, dex_addr.as_str(), third_fee_tier.clone()).unwrap();
+    add_fee_tier!(app, dex, first_fee_tier, "alice").unwrap();
+    add_fee_tier!(app, dex, second_fee_tier, "alice").unwrap();
+    add_fee_tier!(app, dex, third_fee_tier, "alice").unwrap();
 
-    let fee_tiers: Vec<FeeTier> = mock_app.query(dex_addr.clone(), &QueryMsg::FeeTiers {}).unwrap();
+    let fee_tiers: Vec<FeeTier> = app.query(dex.clone(), &QueryMsg::FeeTiers {}).unwrap();
     assert!(fee_tiers.contains(&first_fee_tier));
     assert!(fee_tiers.contains(&second_fee_tier));
     assert!(fee_tiers.contains(&third_fee_tier));
@@ -29,78 +26,64 @@ fn test_add_multiple_fee_tiers() {
 
 #[test]
 fn test_add_fee_tier_not_admin() {
-    let mut mock_app = MockApp::new(&[("admin", &[]), ("user", &[])]);
-    let admin = "admin";
-    let user = "user";
-
-    let protocol_fee = Percentage::new(0);
-    let dex_addr = mock_app.create_dex(admin, protocol_fee).unwrap();
-
-    let fee_tier = FeeTier::new(Percentage::new(1), 1).unwrap();
-    let result = mock_app.add_fee_tier(user, dex_addr.as_str(), fee_tier).unwrap_err();
-    assert!(result.contains("error executing WasmMsg"));
+    let admin = "alice";
+    let not_admin = "user";
+    let mut app = MockApp::new(&[(admin, &[]), ("user", &[])]);
+    let dex = create_dex!(app, Percentage::new(0));
+    let result = add_fee_tier!(
+        app,
+        dex,
+        FeeTier::new(Percentage::new(1), 1).unwrap(),
+        not_admin
+    );
+    assert!(result.is_err());
 }
 
 #[test]
 fn test_add_fee_tier_zero_fee() {
-    let mut mock_app = MockApp::new(&[("admin", &[])]);
-    let admin = "admin";
-
-    let protocol_fee = Percentage::new(0);
-    let dex_addr = mock_app.create_dex(admin, protocol_fee).unwrap();
-
-    let fee_tier = FeeTier::new(Percentage::new(0), 10).unwrap();
-    let result = mock_app.add_fee_tier(admin, dex_addr.as_str(), fee_tier);
+    let mut app = MockApp::new(&[("alice", &[])]);
+    let dex = create_dex!(app, Percentage::new(0));
+    let result = add_fee_tier!(
+        app,
+        dex,
+        FeeTier::new(Percentage::new(0), 10).unwrap(),
+        "alice"
+    );
     assert!(result.is_ok());
 }
 
 #[test]
 fn test_add_fee_tier_tick_spacing_zero() {
-    let mut mock_app = MockApp::new(&[("admin", &[])]);
-    let admin = "admin";
-
-    let protocol_fee = Percentage::new(0);
-    let dex_addr = mock_app.create_dex(admin, protocol_fee).unwrap();
-
+    let mut app = MockApp::new(&[("alice", &[])]);
+    let dex = create_dex!(app, Percentage::new(0));
     let fee_tier = FeeTier {
         fee: Percentage::new(1),
         tick_spacing: 0,
     };
-    let result = mock_app.add_fee_tier(admin, dex_addr.as_str(), fee_tier).unwrap_err();
-
-    assert!(result.contains("error executing WasmMsg"));
+    let result = add_fee_tier!(app, dex, fee_tier, "alice");
+    assert!(result.is_err());
 }
 
 #[test]
 fn test_add_fee_tier_over_upper_bound_tick_spacing() {
-    let mut mock_app = MockApp::new(&[("admin", &[])]);
-    let admin = "admin";
-
-    let protocol_fee = Percentage::new(0);
-    let dex_addr = mock_app.create_dex(admin, protocol_fee).unwrap();
-
+    let mut app = MockApp::new(&[("alice", &[])]);
+    let dex = create_dex!(app, Percentage::new(0));
     let fee_tier = FeeTier {
         fee: Percentage::new(1),
         tick_spacing: 101,
     };
-    let result = mock_app.add_fee_tier(admin, dex_addr.as_str(), fee_tier).unwrap_err();
-
-    assert!(result.contains("error executing WasmMsg"));
+    let result = add_fee_tier!(app, dex, fee_tier, "alice");
+    assert!(result.is_err());
 }
 
 #[test]
 fn test_add_fee_tier_fee_above_limit() {
-    let mut mock_app = MockApp::new(&[("admin", &[])]);
-    let admin = "admin";
-
-    let protocol_fee = Percentage::new(0);
-    let dex_addr = mock_app.create_dex(admin, protocol_fee).unwrap();
-
+    let mut app = MockApp::new(&[("alice", &[])]);
+    let dex = create_dex!(app, Percentage::new(0));
     let fee_tier = FeeTier {
         fee: Percentage::new(1000000000000),
         tick_spacing: 10,
     };
-    let result = mock_app.add_fee_tier(admin, dex_addr.as_str(), fee_tier).unwrap_err();
-
-    assert!(result.contains("error executing WasmMsg"));
+    let result = add_fee_tier!(app, dex, fee_tier, "alice");
+    assert!(result.is_err());
 }
--- contracts/oraiswap-v3/src/tests/change_fee_receiver.rs
+++ contracts/oraiswap-v3/src/tests/change_fee_receiver.rs
@@ -1,62 +1,52 @@
 use crate::math::types::percentage::Percentage;
 use crate::math::types::sqrt_price::calculate_sqrt_price;
-use crate::msg::{ExecuteMsg, QueryMsg};
-use crate::tests::helper::MockApp;
-use crate::{FeeTier, Pool, PoolKey};
+use crate::msg::ExecuteMsg;
+use crate::tests::helper::{macros::*, MockApp};
+use crate::{FeeTier, PoolKey};
 use cosmwasm_std::Addr;
 use decimal::Decimal;
 
 #[test]
 fn test_change_fee_reciever() {
-    let mut mock_app = MockApp::new(&[("admin", &[]), ("alice", &[])]);
     let admin = "admin";
-    let alice = "alice";
+    let new_fee_receiver = "alice";
+    let mut app: MockApp = MockApp::new(&[(admin, &[])]);
+    let dex = create_dex!(app, Percentage::new(0));
 
-    let protocol_fee = Percentage::new(0);
-    let dex_addr = mock_app.create_dex(admin, protocol_fee).unwrap();
-
-    let token_x = Addr::unchecked("token_x");
-    let token_y = Addr::unchecked("token_y");
+    let (token_x, token_y) = create_tokens!(app, 0, 0);
     let fee_tier = FeeTier::new(Percentage::new(1), 1).unwrap();
     let init_tick = 0;
     let init_sqrt_price = calculate_sqrt_price(init_tick).unwrap();
 
-    let result = mock_app.add_fee_tier(admin, dex_addr.as_str(), fee_tier.clone());
+    let result = add_fee_tier!(app, dex, fee_tier, admin);
     assert!(result.is_ok());
 
-    let result = mock_app.create_pool(
-        admin,
-        dex_addr.as_str(),
-        token_x.as_str(),
-        token_y.as_str(),
+    let result = create_pool!(
+        app,
+        dex,
+        token_x,
+        token_y,
         fee_tier.clone(),
         init_sqrt_price,
         init_tick,
+        admin
     );
     assert!(result.is_ok());
 
     let pool_key = PoolKey::new(token_x.clone(), token_y.clone(), fee_tier.clone()).unwrap();
-    let result = mock_app.execute(
+    let result = app.execute(
         Addr::unchecked(admin),
-        Addr::unchecked(dex_addr.clone()),
+        Addr::unchecked(dex.clone()),
         &ExecuteMsg::ChangeFeeReceiver {
             pool_key: pool_key.clone(),
-            fee_receiver: Addr::unchecked(alice),
+            fee_receiver: Addr::unchecked(new_fee_receiver),
         },
         &[],
     );
     assert!(result.is_ok());
 
-    let pool: Pool = mock_app.query(
-        dex_addr.clone(),
-        &QueryMsg::Pool {
-            token_0: token_x,
-            token_1: token_y,
-            fee_tier: fee_tier.clone(),
-        },
-    )
-    .unwrap();
-    assert_eq!(pool.fee_receiver, Addr::unchecked(alice));
+    let pool = get_pool!(app, dex, token_x, token_y, fee_tier).unwrap();
+    assert_eq!(pool.fee_receiver, Addr::unchecked(new_fee_receiver));
 }
 
 #[test]
@@ -89,15 +79,16 @@ fn test_not_admin_change_fee_reciever() {
     assert!(result.is_ok());
 
     let pool_key = PoolKey::new(token_x.clone(), token_y.clone(), fee_tier.clone()).unwrap();
-    let result = mock_app.execute(
-        Addr::unchecked(user),
-        Addr::unchecked(dex_addr.clone()),
-        &ExecuteMsg::ChangeFeeReceiver {
-            pool_key,
-            fee_receiver: Addr::unchecked("bob"),
-        },
-        &[],
-    )
-    .unwrap_err();
+    let result = mock_app
+        .execute(
+            Addr::unchecked(user),
+            Addr::unchecked(dex_addr.clone()),
+            &ExecuteMsg::ChangeFeeReceiver {
+                pool_key,
+                fee_receiver: Addr::unchecked("bob"),
+            },
+            &[],
+        )
+        .unwrap_err();
     assert!(result.contains("error executing WasmMsg"));
 }
