2d63463d59e63d4c8c920af8c63774556ce70139
Pham Tu <tubackkhoa@gmail.com>
2024-06-11 14:31:12 +0700

    add build script

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,14 +1,13 @@
-name=oraiswap_v3_wasm
+
 # build web
 wasm-pack build --target web --features tsify/js
-cp pkg/${name}.js pkg/${name}.d.ts pkg/${name}_bg.wasm npm
+cp pkg/oraiswap_v3_wasm.js pkg/oraiswap_v3_wasm.d.ts pkg/oraiswap_v3_wasm_bg.wasm npm
 
 # build nodejs
 wasm-pack build --target nodejs --features tsify/js
-cp pkg/${name}.js npm/${name}_main.js 
-cp pkg/${name}.d.ts npm/${name}_main.d.ts 
-cp pkg/${name}_bg.wasm npm/${name}_bg_main.wasm 
-sed -i '' 's|${name}_bg.wasm|${name}_bg_main.wasm|g' npm/${name}_main.js
+cp pkg/oraiswap_v3_wasm.js npm/oraiswap_v3_wasm_main.js 
+cp pkg/oraiswap_v3_wasm_bg.wasm npm/oraiswap_v3_wasm_bg_main.wasm 
+sed -i '' 's|oraiswap_v3_wasm_bg.wasm|oraiswap_v3_wasm_bg_main.wasm|g' npm/oraiswap_v3_wasm_main.js
 
 # # deploy npm
 # cd npm 
